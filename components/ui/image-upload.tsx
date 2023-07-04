a95ff0d4ab6856e8e07f49de5b81045bb00c0ef5
KarandeepSingh186 <ks.karandeepsingh321@gmail.com>
2023-07-04 07:12:57 +0530

    final

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="vxjdqwqr">
         {({ open }) => {
           const onClick = () => {
             open();
